7594f342ce9bf0397114da49e4007a57e613a7b7
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-15 06:50:51 +0200

    displayed the data of the selected user

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,11 +1,40 @@
 const usersContainer = document.querySelector(".users");
+const postsContainer = document.querySelector("ul");
 
 const request = fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((users) => {
-    users.forEach((user) => {
-      const html = `<article name=${user.id} class="user">${user.name}</article>`;
+    users.forEach((user, index) => {
+      if (index === 0) {
+        var html = `<article name=${user.id} class="user active">${user.name}</article>`;
+      } else {
+        var html = `<article name=${user.id} class="user">${user.name}</article>`;
+      }
+
       usersContainer.insertAdjacentHTML("beforeend", html);
     });
   })
   .catch((error) => console.error(error));
+
+//   Getting posts
+async function getPosts(id) {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+    );
+    const posts = await response.json();
+    postsContainer.innerHTML = "";
+    posts.forEach((post) => {
+      const html = `<li>${post.body}</li>`;
+      postsContainer.insertAdjacentHTML("beforeend", html);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+usersContainer.addEventListener("click", function (e) {
+  getPosts(e.target.attributes.name.nodeValue);
+});
+
+// getPosts(1);
