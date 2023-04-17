c6c126c762406a301364a59ddc566c2b6aa6ad78
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-17 17:52:31 +0200

    implemented an object iterator manually (using Symbol.iterator)

--- Day-3/4-Iterator/iterator.js
+++ Day-3/4-Iterator/iterator.js
@@ -1,14 +1,51 @@
+// const obj = {
+//   name: "Eslam",
+//   age: 24,
+//   address: "10th Of Ramdan City",
+//   [Symbol.iterator]: function* () {
+//     // yield this.name;
+//     // yield this.age;
+//     // yield this.address;
+//     for (let prop in this) {
+//       yield this[prop];
+//     }
+//   },
+// };
+
+// for (prop of obj) {
+//   console.log(prop);
+// }
+
 const obj = {
-  name: "Eslam",
+  name: "Eslam Sobhii",
   age: 24,
-  address: "10th Of Ramdan City",
-  [Symbol.iterator]: function* () {
-    yield this.name;
-    yield this.age;
-    yield this.address;
+  address: "10th Of Ramadan City",
+  [Symbol.iterator]: function () {
+    let self = this;
+    let keys = Object.keys(self);
+    let pointer = 0;
+    return {
+      next: function () {
+        if (pointer < keys.length) {
+          let key = keys[pointer++];
+          return {
+            // value: [key, self[key]],
+            value: self[key],
+            done: false,
+          };
+        } else {
+          return {
+            value: undefined,
+            done: true,
+          };
+        }
+      },
+    };
   },
 };
 
-for (prop of obj) {
+for (let prop of obj) {
   console.log(prop);
 }
+
+console.log(...obj);
