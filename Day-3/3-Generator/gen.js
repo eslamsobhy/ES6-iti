94115e0ae47f56b7186c9fe659fdb2f26a9d6fde
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-17 14:07:39 +0200

    implemented another version of the Fibonacci generator

--- Day-3/3-Generator/gen.js
+++ Day-3/3-Generator/gen.js
@@ -9,9 +9,31 @@ function* Fibonacci1(num) {
   }
 }
 
+function* Fibonacci2(max) {
+  let fib1 = 0;
+  let fib2 = 1;
+  let curFib = 1;
+  while (curFib <= max) {
+    fib1 = fib2;
+    fib2 = curFib;
+    yield curFib;
+    curFib = fib1 + fib2;
+  }
+}
+
+// Testing the first generator
 const gen1 = Fibonacci1(5);
 console.log(gen1.next());
 console.log(gen1.next());
 console.log(gen1.next());
 console.log(gen1.next());
 console.log(gen1.next());
+
+// Testing the second generator
+const gen2 = Fibonacci2(12);
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
